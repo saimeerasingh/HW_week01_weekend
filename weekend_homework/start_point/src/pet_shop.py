6696aa5d366a41b685011675bb3efed16183128d
saimeerasingh <singhsaimeera@gmail.com>
2021-11-19 21:02:22 +0000

    initial set of tests completed

---
+++ weekend_homework/start_point/src/pet_shop.py
@@ -0,0 +1,54 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(shop_name):
+    return shop_name['name']
+
+def get_total_cash(cash):
+    return cash ['admin']['total_cash']
+
+def add_or_remove_cash(cash,money):
+    cash['admin']['total_cash'] += money
+
+def get_pets_sold(dict):
+    return dict['admin']['pets_sold']
+
+def increase_pets_sold(dict,pets):
+    dict['admin']['pets_sold'] += pets
+
+def get_stock_count(dict):
+    return len(dict['pets'])
+
+def get_pets_by_breed(pets,breed_name):
+    new_list =[]
+    for pet in pets['pets']:
+        if pet['breed'] == breed_name:
+            new_list.append(pet['breed'])
+    return new_list
+
+def find_pet_by_name(pets,pet_name):
+    for pet in pets['pets']:
+        if pet['name'] == pet_name:
+            return pet
+
+def remove_pet_by_name(pets,pet_name):
+    for pet in pets['pets']:
+        if pet['name'] == pet_name:
+            pets['pets'].remove(pet)
+    
+def add_pet_to_stock(pet_shop,new_pet):
+    pet_shop['pets'].append('new_pet')
+    return pet_shop
+
+def get_customer_cash(customers):
+    total_cash =0
+    for customer in customers:
+        total_cash += customers['cash']
+        return total_cash
+    
+def remove_customer_cash(customer,amount):
+    customer['cash'] -= int(amount)
+    
+def get_customer_pet_count(customer):
+    return len(customer['pets'])
+    
+def add_pet_to_customer(customer,new_pet):
+    return customer['pets'].append('new_pet')
