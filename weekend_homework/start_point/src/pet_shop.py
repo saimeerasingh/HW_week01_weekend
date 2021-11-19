7c27875aed9314dafdc11614789f2ef4500a1c18
saimeerasingh <singhsaimeera@gmail.com>
2021-11-19 22:52:13 +0000

    integration tests completed

--- weekend_homework/start_point/src/pet_shop.py
+++ weekend_homework/start_point/src/pet_shop.py
@@ -60,13 +60,13 @@ def customer_can_afford_pet(customer,new_pet):
     else:
         return False
 
-def sell_pet_to_customer(pet_shop,pet,customer):
-    # for pet in pet_shop['pets']:
-    #     if pet['name'] == pet:
-    customer['pets'].append(pet)
-            # len(customer['pets'])
-    pet_shop['admin']['pets_sold'] += 1
-    customer['cash'] -= pet['price']
-    pet_shop['admin']['total_cash'] += pet['price']
+def sell_pet_to_customer(pet_shop,pet_sold,customer):
+    for pet in pet_shop['pets']:
+            if pet_sold and pet['name'] == pet_sold['name']:
+                if customer['cash'] >= pet_sold['price']:
+                    customer['pets'].append(pet)
+                    pet_shop['admin']['pets_sold'] += 1
+                    customer['cash'] -= pet_sold['price']
+                    pet_shop['admin']['total_cash'] += pet_sold['price']
 
 
